89127ae5b17be164ac71238ee49741829fa0a5e8
Mordelet Gregoire <ib00895@saphyr.ens.math-info.univ-paris5.fr>
2016-09-25 11:44:32 +0200

    up 42

---
+++ c/42/match/match.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int	match(char *s1, char *s2)
+{
+	if (*s2 == '*')
+		{
+			if (*(s2 + 1) != *(s1 + 1))
+				return (0);
+			return (match(s1 + 1, s2 + 1) || match(s1 + 1, s2));
+		}
+	if (*s1 != *s2)
+		return (0);
+	if (*s1 == 0)
+		return (1);
+	return match(s1 + 1, s2 + 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	if(argc != 3)
+		return (0);
+	printf("%d\n", match(argv[1], argv[2]));
+	return (0);
+}
