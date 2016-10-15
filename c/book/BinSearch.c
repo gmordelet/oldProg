c16d689f24a3fb1765258589a59bda46b660aca1
Mordelet Gregoire <ib00895@saphyr.ens.math-info.univ-paris5.fr>
2016-10-15 17:16:49 +0200

    bgit

--- c/book/BinSearch.c
+++ c/book/BinSearch.c
@@ -6,21 +6,16 @@ int	BinSearch(int x, int v[], int len)
 	
 	low = 0;
 	high = len - 1;
-	mid = high / 2;
+
 	while (low <= high)
 		{
+			mid = (low + high) / 2;
 			if (v[mid] == x)
 				return (1);
 			else if (v[mid] < x)
-				{
-					low = mid + 1;
-					mid = (low + high) / 2;
-				}
+				low = mid + 1;
 			else
-				{
-					high = mid - 1;
-					mid = (low + high) / 2;
-				}
+				high = mid - 1;
 		}
 	return (0);
 }
