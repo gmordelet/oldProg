ac84bb8c45443a9ff4fd8c6a695242accebe877f
Mordelet Gregoire <ib00895@saphyr.ens.math-info.univ-paris5.fr>
2016-10-16 10:03:02 +0200

    bgit

--- c/book/BinSearch.c
+++ c/book/BinSearch.c
@@ -1,5 +1,25 @@
 #include "headers.h"
 
+/* int	BinSearch(int x, int v[], int len) */
+/* { */
+/* 	int low, mid, high; */
+	
+/* 	low = 0; */
+/* 	high = len - 1; */
+
+/* 	while (low <= high) */
+/* 		{ */
+/* 			mid = (low + high) / 2; */
+/* 			if (v[mid] == x) */
+/* 				return (1); */
+/* 			else if (v[mid] < x) */
+/* 				low = mid + 1; */
+/* 			else */
+/* 				high = mid - 1; */
+/* 		} */
+/* 	return (0); */
+/* } */
+
 int	BinSearch(int x, int v[], int len)
 {
 	int low, mid, high;
@@ -7,15 +27,16 @@ int	BinSearch(int x, int v[], int len)
 	low = 0;
 	high = len - 1;
 
-	while (low <= high)
+	mid = high / 2;
+	while (low <= high && v[mid] != x)
 		{
-			mid = (low + high) / 2;
-			if (v[mid] == x)
-				return (1);
-			else if (v[mid] < x)
+			if (v[mid] < x)
 				low = mid + 1;
 			else
 				high = mid - 1;
+			mid = (low + high) / 2;
 		}
+	if (v[mid] == x)
+		return (1);
 	return (0);
 }
--- c/book/test.c
+++ c/book/test.c
@@ -1,28 +1,28 @@
 #include "headers.h"
 
+#define SIZE 800000
+
 int main()
 {
-	int arr1[1000], arr2[1001], i;
+	int arr1[SIZE], arr2[SIZE + 1], i;
 
-	for (i = 0; i < 1000; ++i)
+	for (i = 0; i < SIZE; ++i)
 		arr1[i] = i;
-	for (i = 0; i < 1001; ++i)
+	for (i = 0; i <= SIZE; ++i)
 		arr2[i] = i;
-	for (i = 0; i < 1000; ++i)
+	for (i = 0; i < SIZE; ++i)
 		if (!BinSearch(i, arr1, ArrSize(arr1)))
 			printf("FAIL\n");
-	for (i = 0; i < 1001; ++i)
+	for (i = 0; i <= SIZE; ++i)
 		if (!BinSearch(i, arr2, ArrSize(arr2)))
 			printf("FAIL\n");
 	if (BinSearch(-1, arr1, ArrSize(arr1)))
 		printf("FAIL\n");
-	if (BinSearch(1001, arr1, ArrSize(arr1)))
+	if (BinSearch(SIZE, arr1, ArrSize(arr1)))
 		printf("FAIL\n");
 	if (BinSearch(-1, arr2, ArrSize(arr2)))
 		printf("FAIL\n");
-	if (BinSearch(1002, arr2, ArrSize(arr2)))
+	if (BinSearch(SIZE + 1, arr2, ArrSize(arr2)))
 		printf("FAIL\n");
-	
-
 	return (0);
 }
