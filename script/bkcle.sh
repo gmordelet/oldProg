3d7377d75a20f278589729a22ae56c772cb81f00
Mordelet Gregoire <ib00895@saphyr.ens.math-info.univ-paris5.fr>
2016-10-30 12:09:09 +0100

    bkcle.sh

---
+++ script/bkcle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+src=$HOME/git/oldProg/script/src
+dest=$HOME/git/oldProg/script/dest
+c_date=$(date "+%y%m%d%H%M%S")
+
+dir_count=$(ls $dest -p | tr ' ' '\n' | grep '/' | wc -l)
+if [ $dir_count -lt 3 ]
+then
+	mkdir -v $dest/$c_date
+	rsync -arv $src $dest/$c_date
+else
+	first=$(ls $dest | sort | head -n 1)
+	mv -v $dest/$first $dest/$c_date
+	rsync -arv --delete $src $dest/$c_date 
+fi
